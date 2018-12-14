e14ea2149ecd21bd72f838bc3ff3f023b5d3f01a
juguanghui <juguanghui@megvii.com>
2018-12-14 16:31:14 +0800

    find all blog url

--- src/test/java/PageViewRefresh.java
+++ src/test/java/PageViewRefresh.java
@@ -1,55 +1,45 @@
 
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PageViewRefresh {
 
     public static void main(String[] args) throws Exception {
-        // TODO Auto-generated method stub
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
 //		System.setProperty("webdriver.firefox.bin",
 //				"E:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
 //		System.setProperty("webdriver.firefox.marionette",
 //				"D:\\新建文件夹\\geckodriver.exe");
 //		WebDriver driver=new FirefoxDriver();
-        WebDriver driver = new ChromeDriver();
 //		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 //        String url = "http://10120275.blog.51cto.com//10110275/1954601";
-        ArrayList<String> urls = new ArrayList(Arrays.asList("http://10120275.blog.51cto.com/10110275/1954601",
-                "http://10120275.blog.51cto.com/10110275/1954598",
-                "http://10120275.blog.51cto.com/10110275/1953406",
-                "http://10120275.blog.51cto.com/10110275/1951929",
-                "http://10120275.blog.51cto.com/10110275/1951711",
-                "http://10120275.blog.51cto.com/10110275/1919698",
-                "http://10120275.blog.51cto.com/10110275/1915562",
-                "http://10120275.blog.51cto.com/10110275/1908363",
-                "http://10120275.blog.51cto.com/10110275/1866856",
-                "http://10120275.blog.51cto.com/10110275/1865792",
-                "http://10120275.blog.51cto.com/10110275/1865479",
-                "http://10120275.blog.51cto.com/10110275/1855801",
-                "http://10120275.blog.51cto.com/10110275/1851106",
-                "http://10120275.blog.51cto.com/10110275/1845383",
-                "http://10120275.blog.51cto.com/10110275/1844461",
-                "http://10120275.blog.51cto.com/10110275/1830831",
-                "http://10120275.blog.51cto.com/10110275/1793736",
-                "http://10120275.blog.51cto.com/10110275/1792600",
-                "http://10120275.blog.51cto.com/10110275/1783506",
-                "http://10120275.blog.51cto.com/10110275/1764536",
-                "http://10120275.blog.51cto.com/10110275/1764526",
-                "http://10120275.blog.51cto.com/10110275/1752503",
-                "http://10120275.blog.51cto.com/10110275/1748513",
-                "http://10120275.blog.51cto.com/10110275/1744763",
-                "http://10120275.blog.51cto.com/10110275/1744648",
-                "http://10120275.blog.51cto.com/10110275/1907275",
-                "http://10120275.blog.51cto.com/10110275/1905936",
-                "http://10120275.blog.51cto.com/10110275/1905930",
-                "http://10120275.blog.51cto.com/10110275/1898335",
-                "http://10120275.blog.51cto.com/10110275/1879569",
-                "https://www.meipian.cn/nyugqpg?from=timeline&isappinstalled=0"));
+
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        ArrayList<String> pageUrl = new ArrayList();
+        pageUrl.add("http://blog.51cto.com/10120275/p1");
+        pageUrl.add("http://blog.51cto.com/10120275/p2");
+        pageUrl.add("http://blog.51cto.com/10120275/p3");
+        pageUrl.add("http://blog.51cto.com/10120275/p4");
+
+        ArrayList<String> urls = new ArrayList<String>();
+
+        for (String page : pageUrl) {
+            driver.get(page);
+            List<WebElement> list1 = ((ChromeDriver) driver).findElementsByClassName("tit");
+            for (int i = 0; i < list1.size(); i++) {
+                String url = list1.get(i).getAttribute("href");
+                urls.add(url);
+            }
+        }
 
         for(int i = 0; i < 4000; i++){
             try {
